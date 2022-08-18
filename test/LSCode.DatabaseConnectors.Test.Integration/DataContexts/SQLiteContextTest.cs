8d2671831868e1201acdbb5242cc25c9855ee29b
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2022-08-18 09:54:52 -0300

    SQLiteContextTest

--- test/LSCode.DatabaseConnectors.Test.Integration/DataContexts/SQLiteContextTest.cs
+++ test/LSCode.DatabaseConnectors.Test.Integration/DataContexts/SQLiteContextTest.cs
@@ -1,30 +1,21 @@
-﻿using Dapper;
-using LSCode.DatabaseConnectors.DataContexts;
+﻿using LSCode.DatabaseConnectors.DataContexts;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Data;
-using System.Data.SqlClient;
 
 namespace LSCode.DatabaseConnectors.Test.Integration.DataContexts
 {
-    internal class SQLServerContextTest
+    internal class SQLiteContextTest
     {
-        private readonly string _connectionStringKey = "ConnectionStringSQLServer";
-        private readonly string _connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=LSCode.DatabaseConnectors.Test;Data Source=SANTOS-PC\SQLEXPRESS;";
+        private readonly string _connectionStringKey = "ConnectionStringSQLite";
+        private readonly string _connectionString = $@"Data Source={AppDomain.CurrentDomain.BaseDirectory}\LSCode.DatabaseConnectors.Test.sqlite;Version=3;";
 
-        public SQLServerContextTest()
+        public SQLiteContextTest()
         {
-            var connectionStringMaster = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=master;Data Source=SANTOS-PC\SQLEXPRESS;";
-
-            using var connection = new SqlConnection(connectionStringMaster);
+            var connection = new System.Data.SQLite.SQLiteConnection(_connectionString);
             connection.Open();
-
-            var query = @"USE [master]
-                          IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'LSCode.DatabaseConnectors.Test')
-                          CREATE DATABASE [LSCode.DatabaseConnectors.Test]";
-
-            connection.Execute(query);
             connection.Close();
         }
 
@@ -34,7 +25,7 @@ namespace LSCode.DatabaseConnectors.Test.Integration.DataContexts
             var configuration = new Mock<IConfiguration>();
             configuration.SetupGet(x => x[It.Is<string>(s => s == _connectionStringKey)]).Returns(_connectionString);
 
-            var dataContext = new SQLServerContext(configuration.Object);
+            var dataContext = new SQLiteContext(configuration.Object);
 
             TestContext.WriteLine($"Connection: {dataContext.Connection.State}");
 
@@ -47,7 +38,7 @@ namespace LSCode.DatabaseConnectors.Test.Integration.DataContexts
             var configuration = new Mock<IConfiguration>();
             configuration.SetupGet(x => x[It.Is<string>(s => s == _connectionStringKey)]).Returns(_connectionString);
 
-            var dataContext = new SQLServerContext(configuration.Object);
+            var dataContext = new SQLiteContext(configuration.Object);
 
             dataContext.Dispose();
 
