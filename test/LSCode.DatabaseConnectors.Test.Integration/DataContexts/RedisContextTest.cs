dded70d8231ff80f5fb8fa7e44ad10abcf163da0
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2022-08-18 10:03:04 -0300

    RedisContextTest

---
+++ test/LSCode.DatabaseConnectors.Test.Integration/DataContexts/RedisContextTest.cs
@@ -0,0 +1,41 @@
+﻿using LSCode.DatabaseConnectors.DataContexts;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+
+namespace LSCode.DatabaseConnectors.Test.Integration.DataContexts
+{
+    internal class RedisContextTest
+    {
+        private readonly string _connectionStringKey = "ConnectionStringRedis";
+        private readonly string _connectionString = $@"localhost";
+
+        [Test]
+        public void Constructor_Valid()
+        {
+            var configuration = new Mock<IConfiguration>();
+            configuration.SetupGet(x => x[It.Is<string>(s => s == _connectionStringKey)]).Returns(_connectionString);
+
+            var dataContext = new RedisContext(configuration.Object);
+
+            TestContext.WriteLine($"Connection: {dataContext.Connection.IsConnected}");
+
+            Assert.That(dataContext.Connection.IsConnected, Is.True);
+        }
+
+        [Test]
+        public void Dispose_Success()
+        {
+            var configuration = new Mock<IConfiguration>();
+            configuration.SetupGet(x => x[It.Is<string>(s => s == _connectionStringKey)]).Returns(_connectionString);
+
+            var dataContext = new RedisContext(configuration.Object);
+
+            dataContext.Dispose();
+
+            TestContext.WriteLine($"Connection: {dataContext.Connection.IsConnected}");
+
+            Assert.That(dataContext.Connection.IsConnected, Is.False);
+        }
+    }
+}
